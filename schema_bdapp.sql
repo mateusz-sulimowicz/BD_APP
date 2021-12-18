296b01aa8adcd024a6806d1dc78c39289ee5790b
Mateusz Sulimowicz <mateusz.sulimowicz@syncron.com>
2021-12-18 12:57:03 +0100

    Add database schema
    Signed-off-by: Mateusz Sulimowicz <mateusz.sulimowicz@syncron.com>

---
+++ schema_bdapp.sql
@@ -0,0 +1,81 @@
+drop table product cascade;
+drop table recipe cascade;
+drop table module cascade;
+drop table item cascade;
+drop table product_order cascade;
+drop table config cascade;
+drop table option cascade;
+
+create table product
+(
+    id         integer generated always as identity primary key,
+    name       varchar not null unique,
+    base_price numeric not null
+);
+
+create table module
+(
+    id   integer generated always as identity primary key,
+    name varchar not null
+);
+
+create table item
+(
+    id   integer generated always as identity primary key,
+    name varchar not null
+);
+
+create table product_order
+(
+    id         integer generated always as identity primary key,
+    product_id integer not null references product,
+    deadline   date    not null
+);
+
+create table config
+(
+    order_id  integer not null,
+    module_id integer not null,
+    item_id   integer not null,
+    constraint config_pk PRIMARY KEY (order_id, module_id)
+);
+
+create table option
+(
+    product_id integer not null references product,
+    module_id integer not null references module,
+    item_id   integer not null references item,
+    price     numeric not null,
+    count     integer not null,
+    constraint option_pk primary key (product_id, module_id, item_id)
+);
+
+create table recipe
+(
+    product_id integer not null references product,
+    module_id  integer not null references module,
+    constraint recipe_pk PRIMARY KEY (product_id, module_id)
+);
+
+INSERT INTO product values (default, 'Iphone 7 Plus', 999);
+INSERT INTO product values (default, 'Iphone 10 Plus', 1100);
+INSERT INTO product values (default, 'Iphone 10 MAX PRO', 1300);
+INSERT INTO product values (default, 'Iphone 6S', 599);
+
+INSERT INTO module values (default, 'Storage');
+INSERT INTO module values (default, 'Camera');
+
+INSERT INTO item values (default, 'Storage 512 GB');
+INSERT INTO item values (default, 'Storage 256 GB');
+INSERT INTO item values (default, 'Front Camera 16 Mpx');
+INSERT INTO item values (default, 'Back Camera 32 Mpx');
+
+select * from product;
+select * from module;
+select * from item;
+
+INSERT INTO option values (1, 1, 1, 0, 1);
+INSERT INTO option values (1, 1, 2, 299, 1);
+
+INSERT INTO recipe values (1, 1);
+INSERT INTO recipe values (1, 2);
