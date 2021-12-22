77d10a6497219bf3bc4a157723a1a26bf80eac3a
Mateusz Sulimowicz <ms429603@students.mimuw.edu.pl>
2021-12-23 00:11:19 +0100

    Remove @Entity annotations
    Signed-off-by: Mateusz Sulimowicz <ms429603@students.mimuw.edu.pl>

--- src/main/java/com/mati/domain/item/Item.java
+++ src/main/java/com/mati/domain/item/Item.java
@@ -1,25 +1,19 @@
 package com.mati.domain.item;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name = Item.TABLE_NAME)
-@Getter
-@Setter
-@ToString
+@Data
 public class Item {
 
     public static final String TABLE_NAME = "item";
 
-    @Id
-    @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "name", nullable = false)
     private String name;
 
 }
--- src/main/java/com/mati/domain/product/Product.java
+++ src/main/java/com/mati/domain/product/Product.java
@@ -26,18 +26,4 @@ public class Product {
 
     private List<Module> modules;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        Product product = (Product) o;
-        return Objects.equals(id, product.id) && Objects.equals(name, product.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
 }
